c61faea51dbc82febe59056dd7f4383c4325eda0
erinn-emilie <erinnemiliek@gmail.com>
2026-02-20 22:23:45 -0500

    Fixing up script.js a little.

--- Backend/main.py
+++ Backend/main.py
@@ -3,12 +3,15 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://127.0.0.1:8000"
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:64101",
+    "http://localhost:8000",
+    "http://127.0.0.1:5500"
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
@@ -19,8 +22,8 @@ async def root():
     message = "SQentineL rocks!!!"
     return {"message": message}
 
-@app.get("/query/{query_str}")
-async def read_query(query_str):
+@app.post("/query")
+async def read_query():
     #here we see if the query is good or bad
     message = "Hopefully you know good development practices because we can't check your queries yet!"
     return {"message": message}
--- Frontend/script.js
+++ Frontend/script.js
@@ -1,13 +1,20 @@
 document.getElementById("sendBtn").addEventListener("click", function () {
-    const api_url = 'http://localhost:8000/api/data'; // Replace with your backend URL
 
-    fetch(api_url)
+
+    const api_url = 'http://localhost:8000/query';
+
+    data = "Malicious SQL Query"
+
+    fetch(api_url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Access-Control': 'Allow-Origin'
+        },
+        body: JSON.stringify(data)
+    })
         .then(response => response.json())
         .then(data => {
-            console.log('Success:', data);
-            // Update your UI with the data
+            console.log(data.message);
         })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+})
